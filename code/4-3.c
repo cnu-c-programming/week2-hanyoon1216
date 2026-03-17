8faa75c8cfd572d9d13f5ec31263a30f6cf3ac9e
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-17 19:00:57 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int a, i; 
+    scanf("%d", &a);
 
+    int j = 0;
+    for (i = 2; i < a; i++) {
+        int cul = a % i;
+        if (cul == 0){
+            j = 1;
+            break;
+        }
+    }
+    if(j==1){
+        printf("false");
+    }
+    else
+        printf("true");
+
+    
     return 0;
 }
-
