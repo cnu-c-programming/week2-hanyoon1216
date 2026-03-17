45a45feb68cc4b0447e00d2a096d47f8b15cb571
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-17 17:34:28 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,25 @@
 
 int main()
 {
+    int a, b, cul;
+    char op;
 
+    scanf("%d %d %c", &a, &b, &op);
+    
+    if (op == '+') {
+        cul = a + b;
+    }
+    else if (op == '-') {
+        cul = a - b;
+    }
+    else if (op == '*') {
+        cul = a * b;
+    }
+    else if (op == '/') {
+        cul = a / b;
+    }
+    
+    printf("%d\n", cul);
+    
     return 0;
 }
-
