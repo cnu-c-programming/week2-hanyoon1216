07bd4beebe1ade2efd2b752b100c10c206038c80
hanyoon1216 <hanyoon1216@gmail.com>
2026-03-17 14:24:23 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -7,7 +7,7 @@ int main()
     printf("int: %ld\n", sizeof(int));
     printf("unsigned int: %ld\n", sizeof(unsigned int));
     printf("long: %ld\n", sizeof(long));
-    printf("long long: %lld\n", sizeof(long long));
+    printf("long long: %ld\n", sizeof(long long));
     printf("float: %ld\n", sizeof(float));
 
     return 0;
